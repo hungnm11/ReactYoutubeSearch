28319f72acc61af0100888d61abec9cf70bf91a1
Hung Nguyen <hung@bacoor.co>
2017-07-19 22:08:32 +0700

    update code

---
+++ src/components/search_bar.js
@@ -0,0 +1,32 @@
+import React, {Component} from 'react';
+
+
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      term: ''
+    };
+  }
+
+  onInputChange(term) {
+    this.setState({term});
+    this.props.onSearchTermChange(term);
+  }
+
+  render () {
+    console.log('POPR', this.props);
+    return (
+      <div className="search-bar">
+        <input 
+          value={this.state.term}
+          onChange={event => this.onInputChange(event.target.value)}
+        />
+      </div>
+    )
+  }
+
+}
+
+export default SearchBar;
